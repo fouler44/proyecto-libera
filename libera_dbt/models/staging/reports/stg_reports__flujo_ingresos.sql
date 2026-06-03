3cf46fcfc4f497d18e570fa1aca8fb95a48cb1d7
fouler44 <angel.urbano1803@gmail.com>
2026-06-02 18:55:56 -0600

    Staging fase 1

---
+++ libera_dbt/models/staging/reports/stg_reports__flujo_ingresos.sql
@@ -0,0 +1,21 @@
+SELECT
+    id_venta,
+    TRIM(STATUSINGRESO) AS status_ingreso,
+    TRIM(STATUSVENTA) AS status_venta,
+    TRIM(FOLIO) AS folio,
+    CAST(FECHA_INGRESO AS DATE) AS fecha_ingreso,
+    CAST(FECHA_ARMOTIZACION AS DATE) AS fecha_amortizacion,
+    TRIM(UPPER(DESARROLLOLARGO)) AS desarrollo_largo,
+    TRIM(UPPER(DESARROLLOCORTO)) AS desarrollo_corto,
+    TRIM(UNIDAD) AS unidad,
+    TRIM(ETAPA) AS etapa,
+    TRIM(UPPER(CLIENTE)) AS cliente,
+    TRIM(UPPER(BANCO)) AS banco,
+    TRIM(FORMAPAGO) AS forma_pago,
+    TRIM(CONCEPTO) AS concepto,
+    TRIM(REFERENCIAINGRESOS) AS referencia_ingresos,
+    MONTOPAGADO AS monto_pagado,
+    TRIM(UPPER(STATUS_TERCERO)) AS status_tercero,
+    TRIM(UPPER(NOMBRETERCERO)) AS nombre_tercero,
+    FECHA_CAPTURA AS fecha_captura
+FROM {{ source('raw', 'rp_flujo_ingresos') }}
